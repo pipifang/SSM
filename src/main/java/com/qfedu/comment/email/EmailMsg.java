aad8898b56acc0caeed711d0f800e4c646c3f174
Administrator <1658119598@qq.com>
2018-12-26 14:34:44 +0800

    工具类的添加

---
+++ src/main/java/com/qfedu/comment/email/EmailMsg.java
@@ -0,0 +1,32 @@
+package com.qfedu.comment.email;
+
+
+public class EmailMsg {
+    private String Title;
+    private String Content;
+    private String Email;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
---
+++ src/main/java/com/qfedu/comment/test/PasswordTest.java
@@ -0,0 +1,27 @@
+package com.qfedu.comment.test;
+
+
+import com.qfedu.comment.constant.SystemCon;
+import com.qfedu.comment.util.EncrypUtil;
+
+/**
+ *@Author feri
+ *@Date Created in 2018/11/29 14:11
+ */
+public class PasswordTest {
+    public static void main(String[] args) {
+        String key= EncrypUtil.createAesKey();
+        System.out.println(key);
+        String res="HelloWord";
+        //加密
+        String es=EncrypUtil.encAesStr(SystemCon.TOKENKEY,res);
+        System.out.println("1加密："+es);
+        //解密
+        System.out.println("1解密："+EncrypUtil.decAesStr(SystemCon.TOKENKEY,es));
+        //加密
+        String es2=EncrypUtil.encAesStr(SystemCon.PASSKEY,res);
+        System.out.println("2加密："+es2);
+        //解密
+        System.out.println("2解密："+EncrypUtil.decAesStr(SystemCon.PASSKEY,es2));
+    }
+}
